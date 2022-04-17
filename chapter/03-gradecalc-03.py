fbd793a37b4835ad79177710a07814385a9d5e0c
Vinit Upadhyay <vinitupadhyay8454@gmail.com>
2022-04-18 01:28:26 +0530

    python

---
+++ chapter/03-gradecalc-03.py
@@ -0,0 +1,19 @@
+marks = int(input("enter marks :- "))
+
+if marks>=90:
+    grade = "Ex"
+elif marks>=80:
+    grade = "A"
+elif marks>=70:
+    grade = "B"
+elif marks>=60:
+    grade = "c"
+elif marks>=50:
+    grade = "D"
+else:
+    grade = "F"
+
+print("your Marks is " + grade)
+
+
+
